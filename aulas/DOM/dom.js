a917f29d1d8ef3fb4531e6043880dbd13069444c
mariana macedo <danilo.rt@outlook.com>
2022-07-10 22:16:32 -0300

    continuando curso

--- aulas/DOM/dom.js
+++ aulas/DOM/dom.js
@@ -38,7 +38,7 @@ const paragraphs3 = document.querySelectorAll("p");
 // });
 
 const div = document.querySelector(".content");
-// div.innerHTML += "<h2> Novo H2, inserido com o innerHTML <h2/>";
+// div.innerHTML += "<h2> Novo H2, inserido com o innerHTML </h2>";
 
 const people = ["Jhonatan", "Vinícius", "Diego"];
 // people.forEach((person) => {
@@ -66,3 +66,50 @@ const paragraph4 = document.querySelector(".errorMsg");
 paragraph4.setAttribute("class", "success");
 
 paragraph4.setAttribute("style", "color:green;");
+
+const title2 = document.querySelector(".title2");
+
+// O setAttribute sobrescreve o estilo CSS da propriedade
+// title2.setAttribute("style", "background-color: yellow");
+
+// Para adicionar estilo CSS sem perder o já existente, use a propriedade style
+title2.style.background = "blue";
+// console.log(title2.style);
+// console.log(title2.style.color);
+
+title2.style.marginLeft = "20px";
+title2.style.fontSize = "20px";
+
+// Remover estilo
+title2.style.marginLeft = "";
+
+// Obter, adicionar, remover e alternar classes CSS
+const title3 = document.querySelector("h2");
+// console.log(title3.classList);
+
+// Adicionando e removendo classes
+title3.classList.add("error-title");
+title.classList.remove("error-title");
+title3.classList.add("success-title");
+
+// Adicionando classes dinamicamente
+const spans = document.querySelectorAll("span");
+
+spans.forEach((span) => {
+  if (span.textContent.includes("error")) {
+    span.classList.add("error-title");
+    return;
+  }
+  if (span.textContent.includes("success")) {
+    span.classList.add("success-title");
+    return;
+  }
+
+  span.style.color = "blue";
+});
+
+// Alternando classes
+// Adiciona a classe, se ela ainda não existir
+// Remove a classe, se ela já existir
+title3.classList("test"); // Não existia, então foi adicionada
+title3.classList("test"); // Existia, então foi removida
--- exercicios/exercicio-14/app.js
+++ exercicios/exercicio-14/app.js
@@ -5,7 +5,8 @@
     maiúsculas.
 */
 
-
+const title = document.querySelector("h1");
+title.textContent = title.textContent.toUpperCase();
 
 /*
   02
@@ -14,7 +15,14 @@
   - Cada número deve estar dentro de uma <li> com a classe "number".
 */
 
-const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
+const numbers = [53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55];
+const ul = document.querySelector(".numbers");
+
+const insertNumberIntoUl = (number) => {
+  ul.innerHTML += `<li class="number"> ${number} </li>`;
+};
+
+numbers.forEach(insertNumberIntoUl);
 
 /*
   03
@@ -24,7 +32,18 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     - Se o número é ímpar, exiba-o na cor "pink".
 */
 
+const liS = document.querySelectorAll("li");
 
+liS.forEach((li) => {
+  const isEven = Number(li.textContent % 2 === 0);
+
+  if (isEven) {
+    li.style.color = "lightblue";
+    return;
+  }
+
+  li.style.color = "pink";
+});
 
 /*
   04
@@ -34,7 +53,8 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   P.s: a classe "body-background" já está declarada no style.css.
 */
 
-
+const body = document.querySelector("body");
+body.classList.add("body-background");
 
 /*
   05
@@ -44,7 +64,11 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     do link do index.html.
 */
 
-
+const link = document.querySelector("a");
+link.setAttribute(
+  "href",
+  "https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo"
+);
 
 /*
   06
@@ -52,7 +76,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Exiba o novo valor do atributo href do link no console.
 */
 
-
+console.log(link.getAttribute("href"));
 
 /*
   07
@@ -61,7 +85,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
     manipuladas via JS no h1.
 */
 
-
+console.log(title.style);
 
 /*
   08
@@ -69,7 +93,7 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Remova a classe "body-background", do elemento body.
 */
 
-
+body.classList.remove("body-background");
 
 /*
   09
@@ -77,3 +101,5 @@ const numbers = [ 53, 24, 3, 8, 1, 6, 57, 80, 77, 98, 55 ]
   - Se o link da página possuir uma classe "link", remova-a;
   - Não utilize o método remove() para fazer isso.
 */
+
+link.classList.toggle("link");
